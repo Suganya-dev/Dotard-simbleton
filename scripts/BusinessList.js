bac6e2e22cf1606bc06a8735a760da75e1b8b129
Suganya <suganprakash0621@gmail.com>
2020-10-18 14:18:46 -0500

    Add the codes and made changes

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -2,9 +2,9 @@ import {useCompanies} from "./BusinessProvider.js"
 import {Company} from "./Business.js"
 const ContentElement = document.querySelector(".Companylist")
 
-const companyList = () => {
+export const companyList = () => {
     const number = useCompanies()
-    ContentElement.innerHTML += "<h2>name</h2>"
+    ContentElement.innerHTML += "<h1>company list</h1>"
 
  
 number.forEach(
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
-import {companyList} from "./BusinessList.js"
+import {companyList} from "./scripts/BusinessList.js"
 
 companyList();
